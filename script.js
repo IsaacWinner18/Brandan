03f52b95f129b6925c137312da7b420bbe381bcf
IsaacWinner18 <winsamlab222@gmail.com>
2024-05-01 07:18:46 -0700

    I added a couple styles, and interactivity to the buttons, changed some images and some other few things

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ var prevScrollpos = window.pageYOffset;
       }
       prevScrollpos = currentScrollPos;
     }
+
+
+const arrival = document.querySelector(".arrival");
+const trending = document.querySelector(".trending");
+const treend = document.querySelector("#treend");
+const arrive = document.querySelector("#arrive");
+
+trending.addEventListener("click", () => {
+treend.style.display = "flex";
+arrive.style.display = "none";
+  if(trending.style.backgroundColor = "white") {
+    trending.style.backgroundColor = "black";
+    trending.style.color = 'white';
+    arrival.style.backgroundColor = 'white'
+    arrival.style.color = 'black'
+  }
+})
+
+arrival.addEventListener("click", () => {
+treend.style.display = "none";
+arrive.style.display = "flex";
+  if(arrival.style.backgroundColor = "white") {
+    trending.style.backgroundColor = "white";
+    trending.style.color = 'black';
+    arrival.style.backgroundColor = 'black'
+    arrival.style.color = 'white'
+  }
+})
+
+
+
